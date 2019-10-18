843e28f184ad25d672296ca7336d4466e58da40d
GoBearsCatsLions <prentiss.iv@gmail.com>
2019-10-17 19:41:34 -0700

    thursday night commit

--- PyPoll.py
+++ PyPoll.py
@@ -40,8 +40,17 @@ winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
 
-
-
+# County tally, can I do this efficiently so that the county vote tally is run at the same time as the candidate count? Check grading rubric. 
+# establish county vote counter
+total_county_counter = 0
+# County options (this is where the county names will be stored)
+county_options = []
+# Declare the empty county tally dictionary (this is where key: county, value = votes cast)
+county_votes = {}
+# Leading county and leading count tracker
+leading_county = ""
+leading_county_count = 0
+leading_county_percentage = 0
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
@@ -55,11 +64,13 @@ with open(file_to_load) as election_data:
 
     # Print each row in the csv file.
     for row in file_reader:
-        # Add to the total vote count.
+        # Add to the total vote count. (and total vote tally though this may be unneccessary since total votes will be the same whether they are for candidate or from county)
         total_votes +=1
-        
-        # Print the candidate name from each row
+        total_county_counter +=1
+
+        # Print the candidate name from each row (and county tally)
         candidate_name = row[2]
+        county_name = row[1]
 
         # If the candidate does not match any existing candidate... 
         if candidate_name not in candidate_options:
@@ -71,6 +82,21 @@ with open(file_to_load) as election_data:
 
         # Add a vote to that candidate's count.
         candidate_votes[candidate_name] +=1
+
+# **************************************************************************************************************************
+
+        # If the county name does not match any existing county ...
+        if county_name not in county_options: 
+            # Add it to the list of counties
+            county_options.append(county_name)
+
+            # Begin tracking that county's tally
+            county_votes[county_name] = 0
+
+        # Add a "vote" to that county's tally
+        county_votes[county_name] +=1
+
+# **************************************************************************************************************************
 
 # Save the results to our text file
 with open(file_to_save, "w") as txt_file:
@@ -86,6 +112,52 @@ with open(file_to_save, "w") as txt_file:
     # Save the final vote count to the text file. 
     txt_file.write(election_results)
 
+# **************************************************************************************************************************
+
+# COUNTY PARTICIPATION CODE
+
+    # Determine the percentage of votes cast in each county by looping through the counts.
+    # Interate through the county list.
+    print("\nCounty Votes:")
+    txt_file.write("\nCounty Votes:\n")
+
+    for county_name in county_votes:
+        # Retrieve vote count of a candidate
+        tally = county_votes[county_name]
+        # Calculate the percentage of votes.
+        tally_percentage = float(tally) / float(total_county_counter) * 100
+        
+        # Name the county results variable
+        county_results = (f"{county_name}: {tally_percentage:0.1f}% ({tally:,})\n")
+
+        # Print the county name and percentage of votes.
+        # Print each county, their vote tally, and percentage to the terminal. 
+        print(county_results)
+
+        # Save the winning county's name to the text file.
+        txt_file.write(county_results)
+        
+        # Determine leading vote count and candidate
+        # Determine if the votes are greater than the winning tally
+        if (tally > leading_county_count) and (tally_percentage > leading_county_percentage):
+            # If true then set leading_county_count = tally and leading percentage = 
+            # tally percentage
+            leading_county_count = tally
+            leading_percentage = tally_percentage
+            # Set the leading county equal to the county's name
+            leading_county = county_name
+
+    leading_county_summary = (
+        f"\n--------------------\n"
+        f"Largest County Turnout: {leading_county}\n"
+        f"---------------------\n")
+    print(leading_county_summary)
+    # Save the leading county's results to the text file.
+    txt_file.write(leading_county_summary)
+
+
+
+# **************************************************************************************************************************
       
     # Determine the percentage of votes for each candidate by looping through the counts.
     # Interate through the candidate list.
@@ -124,6 +196,7 @@ with open(file_to_save, "w") as txt_file:
     print(winning_candidate_summary)
     # Save the winning candidate's results to the text file.
     txt_file.write(winning_candidate_summary)
+
 
 
 
